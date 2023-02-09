130b146da29c586fe73c6d5e0cf921e0e658adc8
Frash <89752022+FahimWeblogicAndCyberTechnologies@users.noreply.github.com>
2023-02-09 09:17:58 +0300

    Add files via upload
    Calculate the number of hours the employee worked. Calculate how many hours are in a full-time working week. Divide the total full-time hours by the hours the employee worked, and multiply that number by the earnings. This is the full-time equivalent salary.

---
+++ wage cal/wage cal/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.IbIGrossPay = New System.Windows.Forms.Label()
+        Me.txtHours = New System.Windows.Forms.TextBox()
+        Me.txtHourlyWage = New System.Windows.Forms.TextBox()
+        Me.btncalculate = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn
+        '
+        Me.btn.AutoSize = True
+        Me.btn.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn.Location = New System.Drawing.Point(130, 73)
+        Me.btn.Name = "btn"
+        Me.btn.Size = New System.Drawing.Size(177, 19)
+        Me.btn.TabIndex = 0
+        Me.btn.Text = "Number Of Hours Worked:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(130, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 19)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Hourly Wage:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(130, 179)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 19)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Gross Pay:"
+        '
+        'IbIGrossPay
+        '
+        Me.IbIGrossPay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.IbIGrossPay.Location = New System.Drawing.Point(370, 179)
+        Me.IbIGrossPay.Name = "IbIGrossPay"
+        Me.IbIGrossPay.Size = New System.Drawing.Size(100, 23)
+        Me.IbIGrossPay.TabIndex = 3
+        '
+        'txtHours
+        '
+        Me.txtHours.Location = New System.Drawing.Point(370, 73)
+        Me.txtHours.Name = "txtHours"
+        Me.txtHours.Size = New System.Drawing.Size(100, 20)
+        Me.txtHours.TabIndex = 4
+        '
+        'txtHourlyWage
+        '
+        Me.txtHourlyWage.Location = New System.Drawing.Point(370, 123)
+        Me.txtHourlyWage.Name = "txtHourlyWage"
+        Me.txtHourlyWage.Size = New System.Drawing.Size(100, 20)
+        Me.txtHourlyWage.TabIndex = 5
+        '
+        'btncalculate
+        '
+        Me.btncalculate.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncalculate.Location = New System.Drawing.Point(134, 258)
+        Me.btncalculate.Name = "btncalculate"
+        Me.btncalculate.Size = New System.Drawing.Size(112, 33)
+        Me.btncalculate.TabIndex = 6
+        Me.btncalculate.Text = "Calculate"
+        Me.btncalculate.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClose.Location = New System.Drawing.Point(370, 258)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(100, 33)
+        Me.btnClose.TabIndex = 7
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btncalculate)
+        Me.Controls.Add(Me.txtHourlyWage)
+        Me.Controls.Add(Me.txtHours)
+        Me.Controls.Add(Me.IbIGrossPay)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btn)
+        Me.Name = "Form1"
+        Me.Text = "Wage Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents IbIGrossPay As Label
+    Friend WithEvents txtHours As TextBox
+    Friend WithEvents txtHourlyWage As TextBox
+    Friend WithEvents btncalculate As Button
+    Friend WithEvents btnClose As Button
+End Class
---
+++ wage cal/wage cal/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btncalculate_Click(sender As Object, e As EventArgs) Handles btncalculate.Click
+        'Variables
+        Dim decHours As Decimal
+        Dim decHourlyWage As Decimal
+        Dim decGrossPay As Decimal
+
+        'Process
+        decHours = CDec(txtHours.Text)
+        decHourlyWage = CDec(txtHourlyWage.Text)
+        decGrossPay = decHours * decHourlyWage
+
+        'Output
+        IbIGrossPay.Text = decGrossPay.ToString("c")
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+End Class
